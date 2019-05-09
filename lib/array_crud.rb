8c13addac9c8d20a5dd4610919638a7fb2276507
Pablo Gonzalez <pabgon0@gmail.com>
2019-05-09 01:34:44 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,12 +8,12 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["1"]
-  array.push("2")
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
   array = ["1"]
-  array.unshift("2")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
