f6bf559e8694a5c99808a6bbde630ba5579288b7
Pablo Gonzalez <pabgon0@gmail.com>
2019-05-09 01:39:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array1 = [element]
-  array = array1.pop
+  array = ["1", "2", "3"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
